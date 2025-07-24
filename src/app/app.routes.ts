8ef9e3ea86469e99c550d42bc26ea8570abbb4d7
mo <moritz.hipper@outlook.de>
2025-07-24 23:14:43 +0200

    fixed routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,17 @@
 import { Routes } from '@angular/router'
 import { CollectionsPageComp } from './components/pages/collections-page-comp/collections-page-comp'
 import { OverviewComp } from './components/pages/overview-page-comp/overview-page-comp'
+import { PracticeComp } from './components/pages/practice-page-comp/practice-page-comp'
 import { SettingsComp } from './components/pages/settings-page-comp/settings-page-comp'
 
 export const routes: Routes = [
   {
     component: OverviewComp,
-    path: ''
+    path: 'cards'
   },
   {
-    component: OverviewComp,
-    path: 'cards'
+    component: PracticeComp,
+    path: 'practice'
   },
   {
     component: CollectionsPageComp,
@@ -22,7 +23,7 @@ export const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'cards',
     pathMatch: 'full'
   }
 ]
