cb602ec67f207c02abd11aad95949e5f5f1590f7
mo <moritz.hipper@outlook.de>
2025-12-15 22:23:17 +0100

    wp

--- src/app/components/shared/onboarding-comp/onboarding-comp.ts
+++ src/app/components/shared/onboarding-comp/onboarding-comp.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core'
+import { Component, inject, signal } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { SettingsStore } from '../../../store/settingsStore'
 import { IconComp } from '../icon-comp/icon-comp'
 
 @Component({
   selector: 'app-onboarding-comp',
-  imports: [IconComp],
+  imports: [IconComp, FormsModule],
   templateUrl: './onboarding-comp.html',
   styleUrl: './onboarding-comp.scss'
 })
 export class OnboardingComp {
   protected readonly activeIndex = signal(0)
+  private readonly _settings = inject(SettingsStore)
+
+  protected apiKey = this._settings.apiKey
 
   next() {
     if (this.activeIndex() < 3) {
@@ -23,5 +28,9 @@ export class OnboardingComp {
     if (this.activeIndex() > 0) {
       this.activeIndex.update((i) => i - 1)
     }
+  }
+
+  updateApiKey(apiKey: string) {
+    this._settings.updateSettings({ apiKey })
   }
 }
